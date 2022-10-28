39a912cbb38e1f2db93f82fdf11baf4bc59aab05
ssp2303 <108412954+ssp2303@users.noreply.github.com>
2022-10-28 18:29:43 +0530

    Update set.py

--- set.py
+++ set.py
@@ -1,17 +1,17 @@
 # Program to perform different set operations like in mathematics
 
-# define three sets
-E = {0, 2, 4, 6, 8};
-N = {1, 2, 3, 4, 5};
+# Define three sets
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
 
-# set union
+# Set Union
 print("Union of E and N is",E | N)
 
-# set intersection
+# Set Intersection
 print("Intersection of E and N is",E & N)
 
-# set difference
+# Set Difference
 print("Difference of E and N is",E - N)
 
-# set symmetric difference
+# Set Symmetric difference
 print("Symmetric difference of E and N is",E ^ N)
